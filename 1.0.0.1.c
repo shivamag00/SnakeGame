d70c38a39d4d734f1d11da71d46167b95145de2b
Shivam Agarwal <44562091+shivamag00@users.noreply.github.com>
2019-02-01 23:29:03 +0530

    Update 1.0.0.1.c

--- 1.0.0.1.c
+++ 1.0.0.1.c
@@ -2,14 +2,15 @@
 //For Moving the '*', press d+enter to go right  
 //For Moving the '*', press s+enter to go down,  
 
+//lets begin with writing the code for right and down movements only
 #include<stdio.h>
-void right(int r)
+void right(int r) // to move "*" right
 {
     while (r--)
     printf(" ");
     printf("*");
 }
-void down(int d, int r)
+void down(int d, int r) // to move "*" down
 {
     printf("\n");
     while (r--)
@@ -20,19 +21,19 @@ void down(int d, int r)
 
 void main()
 {
-    int u=0,d=0,l=0,r=0;
+    int u=0,d=0,l=0,r=0;  //'u' for counting no of up movements, 'd' for down and r for right movements.
     while (1)
     {
         char a,j;
         scanf("%c",&a);
         switch(a)
         {
-            case 'W':
-                u++;
-                break;
-            case 'A':
-                l++;
-                break;
+            //case 'W':
+            //    u++;
+            //    break;
+            //case 'A':
+            //    l++;
+            //    break;
             case 'S':
                 d++;
                 down(d,r);
